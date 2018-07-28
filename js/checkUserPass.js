c4780c97a384970a6e26e30ac41406bfe97a0a5d
ShadowWalkerPro <revniv.to@gmail.com>
2018-07-28 14:53:13 +0200

    sixth commit

---
+++ js/checkUserPass.js
@@ -0,0 +1,27 @@
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'd6F3Efeq';
+
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+ 
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
+
+function checkUserPass(user, pass) {
+    if(user == "" && encrypt(pass) == ""){
+
+    } else if (user == "" && encrypt(pass) == "") {
+
+    } else {
+        
+    }
+}
